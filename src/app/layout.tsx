a5759f2d8a2049299546dc5394ea960532bf500f
Sam Flavin <seamus@Sams-Laptop.local>
2025-03-21 22:01:50 +0800

    prepare to add cart context to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,10 @@ import "./globals.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body >
+      <body>
    
         <Navbar />
-      
+        
         {children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Carousel from "../components/Carousel";
 export default function Home() {
   return (
     <div className="relative w-full h-screen">
+   
       <div className="absolute inset-0 -z-10">
         <Carousel />
       </div>
