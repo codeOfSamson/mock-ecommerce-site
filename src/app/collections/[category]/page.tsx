43cfb8f08060d171acd8a8179c5da116bb442f28
Sam Flavin <seamus@Sams-Laptop.local>
2025-03-23 12:11:05 +0800

    dynamic product page

--- src/app/collections/[category]/page.tsx
+++ src/app/collections/[category]/page.tsx
@@ -42,7 +42,7 @@ export default function CollectionPage() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {filteredProducts.length > 0 ? (
           filteredProducts.map((product, index) => (
-            <Link key={index} href={`/product/${index}`} className="block border p-4 rounded-lg shadow-md hover:shadow-lg">
+            <Link key={index} href={`/product/${product.name}`} className="block border p-4 rounded-lg shadow-md hover:shadow-lg">
               <Image 
                 src={`/images/${product.image}`} 
                 alt={product.name} 
